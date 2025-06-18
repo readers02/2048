c0ccb8a7a3af942f442ed3f9696dc1b325279bb6
Scott Reader <readers@mcmaster.ca>
2025-06-18 16:09:46 -0400

    Add files via upload

---
+++ game2048.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+// CODE: Inlcude necessary librar(y/ies)
+
+int main() {
+    srand(time(NULL)); // set seed to NULL for randomness from time.h
+    // CODE: if necessary
+
+    int grid[4][4];
+    
+    while (1) {
+        // CODE: here we only call the functions
+        // print the board in every iteration
+        // create a function to check if the game is over
+        // call getch_unix(), if the return is 'q' quit the game
+        /// if not send it your game logic
+    }
+    return 0;
+}
