abb9f0d2e4195d2b8357247e55213af4b0320a95
Scott Reader <readers@mcmaster.ca>
2025-06-20 20:38:19 -0400

    Add files via upload

--- game2048.c
+++ game2048.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 // CODE: Inlcude necessary librar(y/ies)
 
 int main() {
@@ -7,7 +9,28 @@ int main() {
     // CODE: if necessary
 
     int grid[4][4];
-    
+
+    int x;
+    int y;
+    int num;
+
+    for (int i; i>=1; i++){
+        x = rand() % 4;
+        y = rand() % 4;
+        if (i == 1){
+            num = (rand()%2+1)*2; //generate a 2 or a 4
+        }else{
+            num = 2;
+        }
+    }
+
+    //func for moving across array sideways
+    for (int y; y<4; y++){
+
+    }
+    //func for moving across array vertically
+
+
     while (1) {
         // CODE: here we only call the functions
         // print the board in every iteration
@@ -16,4 +39,12 @@ int main() {
         /// if not send it your game logic
     }
     return 0;
+}
+
+int[][] negmvmnt(int grid[][]){
+
+}
+
+int[][] posmvmnt(int grid[][]){
+
 }
